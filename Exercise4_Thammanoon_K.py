c98bde4aba9ad784d63578cdebcb4072c3391f12
Poskj <50130793+Poskj@users.noreply.github.com>
2019-05-03 14:05:35 +0700

    Add files via upload

---
+++ Exercise4_Thammanoon_K.py
@@ -0,0 +1,14 @@
+F = 81
+G = 77
+I = 78
+C = 88
+
+print("------Your Score------")
+
+print("Foundation English :",F)
+
+print("General Business :",G)
+
+print("Introduction of Computer Systems :",I)
+
+print("Computer Programming :",C)
