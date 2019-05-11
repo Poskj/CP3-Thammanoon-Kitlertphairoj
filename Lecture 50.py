1712a3a4b430327ae3c7d2e679c6aba04e64a452
Poskj <50130793+Poskj@users.noreply.github.com>
2019-05-11 11:24:34 +0700

    Add files via upload

---
+++ Lecture 50.py
@@ -0,0 +1,13 @@
+def addNumber(x,y):
+    print(x+y)
+def subtractNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+
+addNumber(50,5)
+subtractNumber(50,5)
+multiplyNumber(50,5)
+divideNumber(50,5)
